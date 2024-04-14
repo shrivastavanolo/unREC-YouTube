90b6662d10d03dd2bd9356081849dade60373bc0
Shreya Shrivastava <126598622+shrivastavanolo@users.noreply.github.com>
2024-04-14 16:45:13 +0530

    Add files via upload

---
+++ content.js
@@ -0,0 +1,28 @@
+// content.js
+var child2 = document.querySelector('#related');
+console.log("REC SYSTEM IS HERE");
+if (!child2) {
+  console.log("Tag not found");
+}
+else{
+  console.log("ITS HEREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+  while(document.querySelector('#related').firstChild){
+    document.querySelector('#related').removeChild(document.querySelector('#related').firstChild);
+  }
+
+  // Fetch a random quote from an API endpoint
+  fetch('https://api.quotable.io/random')
+    .then(response => response.json())
+    .then(data => {
+      // Create a new div element to hold the quote;
+      child2.innerHTML = `
+        <p style="font-size: 20px; font-weight: bold; color: white; text-align: center;">${data.content}</p>
+        <p style="font-size: 14px; color: white; text-align: center;">- ${data.author} -</p>
+      `;
+      // Insert the quote container after the video player
+    })
+    .catch(error => {
+      console.error('Error fetching quote:', error);
+    });
+  
+  }
